4e915dba4008597fc3d32803b1388818081313ad
kino-ma <ma@kino.ma>
2024-03-27 11:33:04 +0900

    feat: scheduler by mean GPU usage

---
+++ src/scheduler/mean.rs
@@ -0,0 +1,39 @@
+use crate::{utils::mul_as_percent, ServerInfo};
+
+use super::{
+    interface::DeploymentScheduler,
+    stats::{ServerStats, StatsMap},
+};
+
+pub struct MeanGpuScheduler {}
+
+impl DeploymentScheduler for MeanGpuScheduler {
+    fn schedule(&self, stats_map: &StatsMap) -> Option<&ServerInfo> {
+        let mut least_utilized = stats_map.iter().next()?.1;
+        let mut least_utilized_rate = 0.;
+
+        for (id, stats) in stats_map {
+            let utilized_rate = self.utilized_rate(stats);
+
+            if utilized_rate < least_utilized_rate {
+                least_utilized = stats;
+                least_utilized_rate = utilized_rate;
+            }
+        }
+
+        Some(&least_utilized.server)
+    }
+}
+
+impl MeanGpuScheduler {
+    fn utilized_rate(&self, stats: &ServerStats) -> f64 {
+        let total: f64 = stats.windows().map(|w| w.nanos as f64).sum();
+
+        let utilized: f64 = stats
+            .windows()
+            .map(|w| mul_as_percent(w.nanos, w.utilization.gpu as _) as f64)
+            .sum();
+
+        utilized / total
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use mac_address::{get_mac_address, MacAddress, MacAddressError};
+use prost_types::Timestamp;
 
 pub fn get_mac() -> Result<MacAddress, MacAddressError> {
     match get_mac_address() {
@@ -6,4 +7,23 @@ pub fn get_mac() -> Result<MacAddress, MacAddressError> {
         Ok(None) => Err(MacAddressError::InternalError),
         Err(err) => Err(err),
     }
+}
+
+/// subtract_window computes subtract `end` - `start`.
+/// The result is returned in nanoseconds.
+pub fn subtract_window(end: Timestamp, start: Timestamp) -> i64 {
+    let mut start_i128 = i128::from(start.nanos);
+    start_i128 += (end.seconds << 32) as i128;
+
+    let mut end_i128 = i128::from(end.nanos);
+    end_i128 += (end.seconds << 32) as i128;
+
+    (end_i128 - start_i128) as i64
+}
+
+pub fn mul_as_percent(x: i64, percent: i64) -> i64 {
+    let x = x as f64;
+    let y = percent as f64 / 100.;
+
+    (x * y) as i64
 }
