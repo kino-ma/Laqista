bbf7ed2e581f9384e5b680387333223e76a18fd6
kino-ma <ma@kino.ma>
2024-03-30 04:01:18 +0900

    feat: monitor gpu in another thread

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,22 @@ use clap::Parser;
 
 use mless::{
     cmd::{Cli, Commands},
+    monitor::PowerMonitor,
     server::ServerRunner,
 };
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
+
+    tokio::spawn(async {
+        println!("start thread");
+        let (tx, _rx) = mpsc::channel(1);
+        let monit = PowerMonitor::new();
+        monit.start(tx).await;
+        println!("end thread");
+    });
 
     use Commands::*;
 
--- src/monitor.rs
+++ src/monitor.rs
@@ -1,26 +1,77 @@
-use serde::Serialize;
+use std::{
+    io::{BufRead, BufReader, Lines},
+    marker::PhantomData,
+    process::{self, ChildStdout, Command},
+};
 
-#[derive(Debug, PartialEq, Serialize)]
+use bytes::BytesMut;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use tokio::sync::mpsc;
+
+pub struct PowerMonitor {}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct PowerMetrics {
     pub gpu: Gpu,
 }
 
-#[derive(Debug, PartialEq, Serialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Gpu {
-    pub freq_mhz: f64,
+    pub freq_hz: f64,
     pub dvfm_states: Vec<DvfmState>,
 }
 
-#[derive(Debug, PartialEq, Serialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct DvfmState {
-    pub freq_mhz: u16,
+    pub freq: u16,
+}
+
+impl PowerMonitor {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub async fn start(&self, tx: mpsc::Sender<PowerMonitor>) {
+        println!("start start");
+        let commands = Self::commands();
+
+        loop {
+            println!("loop...");
+            let cmd = Command::new(commands[0])
+                .args(&commands[1..])
+                .stdout(process::Stdio::piped())
+                .spawn()
+                .expect("failed to spawn monitor process");
+
+            let stdout = cmd.stdout.expect("faile to get child's stdout");
+            let reader = BufReader::new(stdout);
+
+            let plists: Plists<PowerMetrics> = Plists::new(reader.lines());
+
+            for metrics in plists {
+                println!("metrics = {:?}", metrics);
+                println!("utilization = {:?}", metrics.gpu.utilization_ratio());
+            }
+            println!("end loop");
+        }
+    }
+
+    fn commands() -> Vec<&'static str> {
+        vec![
+            "/usr/bin/powermetrics",
+            "--sampler=gpu_power",
+            "--sample-rate=3000", // in ms
+            // "--sample-count=1",
+            "--format=plist",
+        ]
+    }
 }
 
 impl Gpu {
     pub fn max_frequency(&self) -> u16 {
         self.dvfm_states
             .iter()
-            .map(|state| state.freq_mhz)
+            .map(|state| state.freq)
             .max()
             .unwrap()
     }
@@ -28,7 +79,7 @@ impl Gpu {
     pub(crate) fn min_frequency(&self) -> u16 {
         self.dvfm_states
             .iter()
-            .map(|state| state.freq_mhz)
+            .map(|state| state.freq)
             .min()
             .unwrap()
     }
@@ -36,8 +87,68 @@ impl Gpu {
     pub fn utilization_ratio(&self) -> f64 {
         let min = self.min_frequency() as f64;
         let max = self.max_frequency() as f64;
-        ((self.freq_mhz - min).max(0.0) / (max - min).max(1.0))
+        ((self.freq_hz - min).max(0.0) / (max - min).max(1.0))
             .max(0.0)
             .min(1.0)
+    }
+}
+
+type StdoutLines = Lines<BufReader<ChildStdout>>;
+struct Plists<T> {
+    inner: StdoutLines,
+    phantom: PhantomData<T>,
+}
+
+impl<T> Plists<T>
+where
+    T: DeserializeOwned,
+{
+    pub fn new(lines: StdoutLines) -> Self {
+        Self {
+            inner: lines,
+            phantom: PhantomData,
+        }
+    }
+
+    fn parse(&self, mut buff: &[u8]) -> Result<T, plist::Error> {
+        if buff[0] == b'\0' {
+            buff = &buff[1..];
+        }
+        plist::from_bytes(buff)
+    }
+}
+
+impl<'a, T> Iterator for Plists<T>
+where
+    T: DeserializeOwned,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buff = BytesMut::new();
+
+        while let Some(line) = self.inner.next() {
+            let line = line.expect("failed to read line");
+
+            println!(
+                "line = '{}' (offset 1 = {:?})",
+                line,
+                line.chars().take(2).collect::<Vec<_>>()
+            );
+
+            buff.extend(line.as_bytes());
+
+            if line == "</plist>" {
+                let parsed = self.parse(&buff);
+                if let Err(e) = &parsed {
+                    println!("WARN: failed to parse plist: {}", e);
+                    println!("data = '{}'", line);
+                }
+
+                return parsed.ok();
+            }
+        }
+
+        None
     }
 }
