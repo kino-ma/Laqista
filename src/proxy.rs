c61210023cadb4c0c4e27ab90b5ef04b17c9ba5b
kino-ma <ma@kino.ma>
2024-03-29 17:14:54 +0900

    feat: capture package name and address

--- src/proxy.rs
+++ src/proxy.rs
@@ -8,10 +8,15 @@ pub async fn create_reverse_proxy(
     package: &str,
     addr: &str,
 ) -> impl Fn() -> Pin<Box<dyn Future<Output = Result<Response<&'static str>, Box<dyn Error>>>>> {
-    || Box::pin(inner_proxy())
+    let package = package.to_owned();
+    let addr = addr.to_owned();
+    move || Box::pin(inner_proxy(package.clone(), addr.clone()))
 }
 
-async fn inner_proxy() -> Result<Response<&'static str>, Box<dyn Error>> {
+async fn inner_proxy(
+    package: String,
+    addr: String,
+) -> Result<Response<&'static str>, Box<dyn Error>> {
     let resp = Response::new("");
     Ok(resp)
 }
