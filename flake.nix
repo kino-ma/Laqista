ba4f0fe846de5c7a08d7a0ff44478899497c793b
kino-ma <ma@kino.ma>
2024-03-30 18:31:19 +0900

    feat: try opencv [wip]

--- flake.nix
+++ flake.nix
@@ -1,22 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, nixpkgs-stable, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
+        pkgs-stable = import nixpkgs-stable { inherit system overlays; };
 
         cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
       in
       {
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ cargo rustc rustfmt rust-analyzer protobuf iconv grpcurl ];
+          nativeBuildInputs = with pkgs; [ cargo rustc rustfmt rust-analyzer protobuf iconv grpcurl pkgs-stable.opencv pkg-config ];
 
           RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
         };
@@ -27,7 +29,7 @@
               inherit (cargoToml.package) name version;
               src = ./.;
               cargoLock.lockFile = ./Cargo.lock;
-              nativeBuildInputs = with pkgs; [ protobuf ];
+              nativeBuildInputs = with pkgs; [ protobuf pkgs-stable.opencv pkg-config ];
             };
 
           default = mless;
