2439a7bf2ecf7336870242d5e5f6747e30d36016
kino-ma <ma@kino.ma>
2024-03-30 14:27:12 +0900

    refactor: switch implementation by target

--- src/monitor/darwin.rs
+++ src/monitor/darwin.rs
@@ -1,24 +1,16 @@
 use std::{
-    io::{BufRead, BufReader, Lines},
-    process::{self, ChildStdout, Command},
-    time::{Duration, SystemTime},
+    io::{BufReader, Lines},
+    process::ChildStdout,
+    time::Duration,
 };
 
 use bytes::BytesMut;
 use plist::Date;
 use serde::{Deserialize, Serialize};
-use tokio::sync::mpsc;
 
 use crate::proto::{MonitorWindow, ResourceUtilization, TimeWindow};
 
-pub struct PowerMonitor {}
-
-#[derive(Clone, Debug)]
-pub struct MetricsWindow {
-    start: SystemTime,
-    end: SystemTime,
-    metrics: PowerMetrics,
-}
+use super::MetricsWindow;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct PowerMetrics {
@@ -37,42 +29,6 @@ pub struct Gpu {
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct DvfmState {
     pub freq: u16,
-}
-
-impl PowerMonitor {
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    pub async fn start(&self, tx: mpsc::Sender<MetricsWindow>) {
-        println!("start start");
-        let commands = Self::commands();
-
-        let cmd = Command::new(commands[0])
-            .args(&commands[1..])
-            .stdout(process::Stdio::piped())
-            .spawn()
-            .expect("failed to spawn monitor process");
-
-        let stdout = cmd.stdout.expect("faile to get child's stdout");
-        let reader = BufReader::new(stdout);
-
-        let plists: MetricsReader = MetricsReader::new(reader.lines());
-
-        for metrics in plists {
-            tx.send(metrics).await.expect("failed to send metrics");
-        }
-    }
-
-    fn commands() -> Vec<&'static str> {
-        vec![
-            "/usr/bin/powermetrics",
-            "--sampler=gpu_power",
-            "--sample-rate=3000", // in ms
-            // "--sample-count=1",
-            "--format=plist",
-        ]
-    }
 }
 
 impl Gpu {
@@ -112,7 +68,7 @@ impl Into<MetricsWindow> for PowerMetrics {
 }
 
 type StdoutLines = Lines<BufReader<ChildStdout>>;
-struct MetricsReader {
+pub struct MetricsReader {
     inner: StdoutLines,
 }
 
