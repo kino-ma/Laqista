373bdae327af391de0f152f07a2fcf22add9ed4c
kino-ma <ma@kino.ma>
2024-04-01 16:09:56 +0900

    test: reuse clients to avoid connection errors
    The benchmark seepms to use up all available sockets

--- src/bench.rs
+++ src/bench.rs
@@ -2,8 +2,8 @@ extern crate test;
 
 #[cfg(test)]
 mod tests {
-    use futures::executor::block_on;
     use test::Bencher;
+    use tokio::runtime;
 
     use crate::{
         app::{self, proto::HelloRequest},
@@ -14,11 +14,17 @@ mod tests {
 
     #[bench]
     fn bench_scheduled(b: &mut Bencher) {
-        let (client, deploy_response) = block_on(async {
-            let mut client =
-                proto::scheduler_client::SchedulerClient::connect("http://127.0.0.1:50051")
-                    .await
-                    .expect("failed to connect to the server");
+        let runtime = runtime::Runtime::new().unwrap();
+        let addr = "http://127.0.0.1:50051";
+
+        let (client, mut app_client, deploy_response) = runtime.block_on(async {
+            let mut client = proto::scheduler_client::SchedulerClient::connect(addr)
+                .await
+                .expect("failed to connect to the server");
+
+            let app_client = app::proto::greeter_client::GreeterClient::connect(addr)
+                .await
+                .unwrap();
 
             let request = DeployRequest {
                 source: "https://github.com/kino-ma/MLess".to_owned(),
@@ -27,29 +33,51 @@ mod tests {
 
             let deployment = client.deploy(request).await.expect("failed to deploy");
 
-            (client, deployment)
+            (client, app_client, deployment)
         });
 
         let deployment = deploy_response.into_inner().deployment.unwrap();
         let deployment_id = deployment.id;
 
-        b.iter(|| async {
-            let request = LookupRequest {
-                deployment_id: deployment_id.clone(),
-                qos: None,
-            };
+        b.iter(|| {
+            runtime.block_on(async {
+                let request = LookupRequest {
+                    deployment_id: deployment_id.clone(),
+                    qos: None,
+                };
 
-            let resp = client.clone().lookup(request).await.unwrap().into_inner();
-            let server = resp.server.unwrap();
-            let addr = server.addr.clone();
+                let _resp = client.clone().lookup(request).await.unwrap().into_inner();
 
-            let mut app_client = app::proto::greeter_client::GreeterClient::connect(addr)
+                // let mut app_client = app::proto::greeter_client::GreeterClient::connect(addr)
+                //     .await
+                //     .unwrap();
+                let request = HelloRequest {
+                    name: "MLess benchamrk".to_owned(),
+                };
+                app_client.say_hello(request).await.unwrap();
+            })
+        })
+    }
+
+    #[bench]
+    fn bench_direct(b: &mut Bencher) {
+        let runtime = runtime::Runtime::new().unwrap();
+        let addr = "http://127.0.0.1:50051";
+
+        let mut app_client = runtime.block_on(async {
+            let app_client = app::proto::greeter_client::GreeterClient::connect(addr)
                 .await
                 .unwrap();
-            let request = HelloRequest {
-                name: "MLess benchamrk".to_owned(),
-            };
-            app_client.say_hello(request).await.unwrap();
+            app_client
+        });
+
+        b.iter(|| {
+            runtime.block_on(async {
+                let request = HelloRequest {
+                    name: "MLess benchamrk".to_owned(),
+                };
+                app_client.say_hello(request).await.unwrap();
+            })
         })
     }
 }
