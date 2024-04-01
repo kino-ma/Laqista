41df280aa041cde59563e441bfb3c725168c7000
kino-ma <ma@kino.ma>
2024-04-01 16:41:57 +0900

    test: pretty-print by default output

--- bench.sh
+++ bench.sh
@@ -6,15 +6,10 @@ output="./logs/$(date '+%Y-%m-%dT%H:%M:%S%z')"
 
 bench() {
     cargo bench -- \
-        -Z unstable-options \
-        --format=json |
+        -Z unstable-options |
         tee "$output"
 }
 
 bench
 
 ln -fs "$(basename $output)" ./logs/latest.log
-
-echo
-echo '=== result ==='
-jq <logs/latest.log 'select(.type == "bench") | .name,.median,"± " + (.deviation|tostring)' | paste - - -
