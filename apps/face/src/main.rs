5286e3e1120d56b48325a993bd72051c93eac82e
kino-ma <ma@kino.ma>
2024-07-05 10:20:25 +0900

    feat: run ONNX detection in main

--- apps/face/src/main.rs
+++ apps/face/src/main.rs
@@ -1,48 +1,21 @@
 use std::error::Error;
 
-use face::{DetectedFrame, Mp4Detector};
+use face::{open_default, DetectionInputs, DetectionOutputs, FaceDetector};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // let maybe_filename = args().nth(1);
-    // let filename = maybe_filename.as_deref().unwrap_or(DEFAULT_VIDEO_FILE);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let frame = open_default();
+    let mut detector = FaceDetector::create_default().await?;
 
-    // let capture = VideoCapture::from_file(filename, videoio::CAP_ANY)?;
-    let capture = VideoCapture::new(0, CAP_ANY).unwrap();
+    let input = frame
+        .as_slice()
+        .expect("failed to convert input image to slice");
+    let input = DetectionInputs { input };
 
-    let window = "video capture";
-    highgui::named_window_def(window)?;
-    let opened = videoio::VideoCapture::is_opened(&capture)?;
-    if !opened {
-        panic!("Unable to open default camera!");
-    }
+    let DetectionOutputs { boxes, .. } = detector.detect(input).await?;
+    let len = boxes.len();
 
-    let detector = Mp4Detector::new(capture);
-
-    for detected_frame in detector {
-        println!("loop");
-        let DetectedFrame { faces, mut frame } = detected_frame;
-
-        println!("faces: {}", faces.len());
-
-        for face in faces.iter() {
-            println!("face {face:?}");
-            let scaled_face =
-                core::Rect::new(face.x * 4, face.y * 4, face.width * 4, face.height * 4);
-            println!("sealed");
-
-            imgproc::rectangle_def(&mut frame, scaled_face, (0, 255, 0).into())?;
-            println!("rected");
-        }
-        println!("inner end");
-
-        println!("faces {faces:?}");
-        highgui::imshow(window, &frame)?;
-        println!("showed");
-        if highgui::wait_key(10)? > 0 {
-            break;
-        }
-        println!("next");
-    }
+    println!("Detected {len} boxes");
 
     Ok(())
 }
