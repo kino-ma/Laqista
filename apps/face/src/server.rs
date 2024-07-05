f65ac8da0b313bf30b0dbe492f20fd7682136dbf
kino-ma <ma@kino.ma>
2024-07-05 10:20:16 +0900

    refactor: move image helpers to lib

--- apps/face/src/server.rs
+++ apps/face/src/server.rs
@@ -1,9 +1,7 @@
-use std::path::{Path, PathBuf};
-
-use image::{imageops::FilterType, ImageBuffer, Pixel as _, Rgb};
 use tonic::{Request, Response, Status};
 
 use crate::{
+    open_default,
     proto::{
         detector_server::Detector, DetectReply, DetectRequest, DetectVideoReply, DetectVideoRequest,
     },
@@ -50,35 +48,4 @@ impl Detector for DetectServer {
         };
         Ok(Response::new(resp))
     }
-}
-
-fn open_default() -> ndarray::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<[usize; 4]>> {
-    let path = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("../../")
-        .join("data/still-people.png");
-
-    open(path)
-}
-
-fn open(path: PathBuf) -> ndarray::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<[usize; 4]>> {
-    let image_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = image::open(path)
-        .unwrap()
-        .resize_exact(28, 28, FilterType::Nearest)
-        .to_rgb8();
-
-    // Python:
-    // # image[y, x, RGB]
-    // # x==0 --> left
-    // # y==0 --> top
-
-    // See https://github.com/onnx/models/blob/master/vision/classification/imagenet_inference.ipynb
-    // for pre-processing image.
-    // WARNING: Note order of declaration of arguments: (_,c,j,i)
-    ndarray::Array::from_shape_fn((1, 1, 28, 28), |(_, c, j, i)| {
-        let pixel = image_buffer.get_pixel(i as u32, j as u32);
-        let channels = pixel.channels();
-
-        // range [0, 255] -> range [0, 1]
-        (channels[c] as f32) / 255.0
-    })
 }
