9918da9e3500c58b7263b470c68b3d485bfa85b4
kino-ma <ma@kino.ma>
2024-07-08 14:48:23 +0900

    chore: add verbose debug logging

--- apps/face/src/main.rs
+++ apps/face/src/main.rs
@@ -4,14 +4,18 @@ use face::{open_default, DetectionInputs, DetectionOutputs, FaceDetector};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    println!("opening frame");
     let frame = open_default();
+    println!("creating detector");
     let mut detector = FaceDetector::create_default().await?;
 
+    println!("creating input");
     let input = frame
         .as_slice()
         .expect("failed to convert input image to slice");
     let input = DetectionInputs { input };
 
+    println!("detecting");
     let DetectionOutputs { boxes, .. } = detector.detect(input).await?;
     let len = boxes.len();
 
