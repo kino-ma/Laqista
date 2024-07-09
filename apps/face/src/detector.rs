be4e7480f3bbd883fb068f72525c703bb5d6d37a
kino-ma <ma@kino.ma>
2024-07-09 14:54:44 +0900

    feat: run squeezed ImageNet

--- apps/face/src/detector.rs
+++ apps/face/src/detector.rs
@@ -1,4 +1,10 @@
-use std::{borrow::Cow, collections::HashMap, error::Error, path::Path};
+use std::{
+    borrow::Cow,
+    collections::HashMap,
+    error::Error,
+    io::{BufRead, BufReader},
+    path::Path,
+};
 
 use wonnx::{
     utils::{InputTensor, OutputTensor},
@@ -31,7 +37,14 @@ impl FaceDetector {
     pub async fn create_default() -> Result<Self, SessionError> {
         let path = Path::new(env!("CARGO_MANIFEST_DIR"))
             .join("models")
-            .join("face_detector_640.onnx");
+            // .join("face_detector_640.onnx");
+            .join("opt-squeeze.onnx");
+
+        // let labels_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        //     .join("models")
+        //     .join("opt-squeeze");
+
+        println!("loading model from {path:?}...");
 
         let session = Session::from_path(path).await?;
 
@@ -46,12 +59,28 @@ impl FaceDetector {
         let tensor = InputTensor::F32(cow);
 
         let mut inputs = HashMap::new();
-        inputs.insert("input".to_owned(), tensor);
+        inputs.insert("data".to_owned(), tensor);
 
         let result = self.session.run(&inputs).await?;
-        let outputs = result.try_into()?;
+        println!("result: {result:?}");
+        let probabilities = result.into_iter().next().unwrap().1;
+        let probabilities: Vec<f32> = probabilities.try_into().unwrap();
+        let mut probabilities = probabilities.iter().enumerate().collect::<Vec<_>>();
+        probabilities.sort_unstable_by(|a, b| b.1.partial_cmp(a.1).unwrap());
 
-        Ok(outputs)
+        let class_labels = get_imagenet_labels();
+
+        for i in 0..10 {
+            println!(
+                "Infered result: {} of class: {}",
+                class_labels[probabilities[i].0], probabilities[i].0
+            );
+            println!("details: {:?}", probabilities[i]);
+        }
+        // let outputs = result.try_into()?;
+
+        // Ok(outputs)
+        panic!()
     }
 }
 
@@ -72,4 +101,14 @@ impl TryFrom<HashMap<String, OutputTensor>> for DetectionOutputs {
             _ => Err("invalid type of scores or boxes".to_owned()),
         }
     }
+}
+
+fn get_imagenet_labels() -> Vec<String> {
+    // Download the ImageNet class labels, matching SqueezeNet's classes.
+    let labels_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("models")
+        .join("squeeze-labels.txt");
+    let file = BufReader::new(std::fs::File::open(labels_path).unwrap());
+
+    file.lines().map(|line| line.unwrap()).collect()
 }
