8275f58ad393d6ce598b42586a27f290c6c67d41
kino-ma <ma@kino.ma>
2024-07-10 15:24:04 +0900

    feat: run abstsract runner in main

--- apps/face/src/main.rs
+++ apps/face/src/main.rs
@@ -1,25 +1,53 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 
-use face::{open_default, DetectionInputs, DetectionOutputs, FaceDetector};
+use face::{open_default, Session};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let model_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("models")
+        .join("opt-squeeze.onnx");
+
     println!("opening frame");
     let frame = open_default();
+
     println!("creating detector");
-    let mut detector = FaceDetector::create_default().await?;
+    let mut session = Session::from_path(path).await?;
 
     println!("creating input");
     let input = frame
         .as_slice()
         .expect("failed to convert input image to slice");
-    let input = DetectionInputs { input };
+    let inputs = HashMap::from([("data", input)]);
 
     println!("detecting");
-    let DetectionOutputs { boxes, .. } = detector.detect(input).await?;
-    let len = boxes.len();
+    let outputs = detector.detect(inputs).await?;
 
-    println!("Detected {len} boxes");
+    println!("result: {result:?}");
+    let probabilities = result.into_iter().next().unwrap().1;
+    let probabilities: Vec<f32> = probabilities.try_into().unwrap();
+    let mut probabilities = probabilities.iter().enumerate().collect::<Vec<_>>();
+    probabilities.sort_unstable_by(|a, b| b.1.partial_cmp(a.1).unwrap());
+
+    let class_labels = get_imagenet_labels();
+
+    for i in 0..10 {
+        println!(
+            "Infered result: {} of class: {}",
+            class_labels[probabilities[i].0], probabilities[i].0
+        );
+        println!("details: {:?}", probabilities[i]);
+    }
 
     Ok(())
+}
+
+fn get_imagenet_labels() -> Vec<String> {
+    // Download the ImageNet class labels, matching SqueezeNet's classes.
+    let labels_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("models")
+        .join("squeeze-labels.txt");
+    let file = BufReader::new(std::fs::File::open(labels_path).unwrap());
+
+    file.lines().map(|line| line.unwrap()).collect()
 }
