5cd9438a80a472bb8c1e5e2ef90b10394718d16d
kino-ma <ma@kino.ma>
2024-08-06 15:07:18 +0900

    feat: utility script to downgrade onnx ir version

---
+++ utils/downgrade.py
@@ -0,0 +1,16 @@
+import sys
+
+import onnx
+from onnx import version_converter
+
+model_path = sys.argv[1]
+onnx_ir_version = int(sys.argv[2])
+output_path = sys.argv[3]
+
+model = onnx.load(model_path)
+
+converted = version_converter.convert_version(model, onnx_ir_version)
+onnx.checker.check_model(converted)
+
+onnx.save(converted, output_path)
+print(f"Saved Softmax graph to {output_path}")
