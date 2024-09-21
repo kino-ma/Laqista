bdd305b3e54212ab41bdb27d98759f2f76f5a89d
kino-ma <ma@kino.ma>
2024-09-22 03:02:14 +0900

    test: switch sent data

--- benches/face-image.rs
+++ benches/face-image.rs
@@ -121,12 +121,12 @@ pub fn bench_wasm(c: &mut Criterion) {
 
     group.bench_with_input(
         BenchmarkId::new("face wasm scheduled", "<client>"),
-        &(arc_client, arc_app_client.clone()),
+        &(arc_client.clone(), arc_app_client.clone()),
         |b, (client, app_client)| {
             b.to_async(Runtime::new().unwrap()).iter(|| async {
                 let mut client = client.lock().await;
                 let mut app_client = app_client.lock().await;
-                run_wasm_scheduled(&mut client, &mut app_client, &deployment_id).await
+                run_wasm_scheduled(&mut client, &mut app_client, &deployment_id, &[2, 40]).await
             })
         },
     );
@@ -137,7 +137,30 @@ pub fn bench_wasm(c: &mut Criterion) {
         |b, app_client| {
             b.to_async(Runtime::new().unwrap()).iter(|| async {
                 let mut app_client = app_client.lock().await;
-                run_wasm_direct(&mut app_client).await
+                run_wasm_direct(&mut app_client, &[2, 40]).await
+            })
+        },
+    );
+
+    group.bench_with_input(
+        BenchmarkId::new("face wasm scheduled full image", "<client>"),
+        &(arc_client.clone(), arc_app_client.clone()),
+        |b, (client, app_client)| {
+            b.to_async(Runtime::new().unwrap()).iter(|| async {
+                let mut client = client.lock().await;
+                let mut app_client = app_client.lock().await;
+                run_wasm_scheduled(&mut client, &mut app_client, &deployment_id, JPEG).await
+            })
+        },
+    );
+
+    group.bench_with_input(
+        BenchmarkId::new("face wasm direct full image", "<client>"),
+        &arc_app_client,
+        |b, app_client| {
+            b.to_async(Runtime::new().unwrap()).iter(|| async {
+                let mut app_client = app_client.lock().await;
+                run_wasm_direct(&mut app_client, JPEG).await
             })
         },
     );
@@ -147,6 +170,7 @@ async fn run_wasm_scheduled(
     client: &mut SchedulerClient<Channel>,
     detector_client: &mut DetectorClient<Channel>,
     deployment_id: &str,
+    image: &[u8],
 ) {
     let request = LookupRequest {
         deployment_id: deployment_id.to_owned(),
@@ -159,14 +183,14 @@ async fn run_wasm_scheduled(
     //     .await
     //     .unwrap();
     let request = DetectionRequest {
-        image_png: JPEG.to_vec(),
+        image_png: image.to_vec(),
     };
     detector_client.run_detection(request).await.unwrap();
 }
 
-async fn run_wasm_direct(detector_client: &mut DetectorClient<Channel>) {
+async fn run_wasm_direct(detector_client: &mut DetectorClient<Channel>, image: &[u8]) {
     let request = DetectionRequest {
-        image_png: JPEG.to_vec(),
+        image_png: image.to_vec(),
     };
     detector_client.run_detection(request).await.unwrap();
 }
