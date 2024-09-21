5a1d94fe5c4356bc63f1ad584c2ea4fdaef982e4
kino-ma <ma@kino.ma>
2024-09-22 02:00:26 +0900

    test: benchmark wasm function

--- benches/face-image.rs
+++ benches/face-image.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use face::proto::detector_client::DetectorClient;
-use face::proto::InferRequest;
+use face::proto::{DetectionRequest, InferRequest};
 use futures::lock::Mutex;
 use tokio::runtime::Runtime;
 use tonic::transport::Channel;
@@ -103,5 +103,68 @@ async fn run_direct(detector_client: &mut DetectorClient<Channel>) {
     detector_client.infer(request).await.unwrap();
 }
 
-criterion_group!(benches, bench_face_image);
+pub fn bench_wasm(c: &mut Criterion) {
+    let addr = "http://127.0.0.1:50051";
+
+    let runtime = Runtime::new().unwrap();
+    let (client, detector_client, deployment) =
+        runtime.block_on(async { setup_clients(addr).await });
+
+    let deployment_id = deployment.id;
+
+    let arc_client = Arc::new(Mutex::new(client));
+    let arc_app_client = Arc::new(Mutex::new(detector_client));
+
+    let mut group = c.benchmark_group("Face wasm");
+
+    group.bench_with_input(
+        BenchmarkId::new("face wasm scheduled", "<client>"),
+        &(arc_client, arc_app_client.clone()),
+        |b, (client, app_client)| {
+            b.to_async(Runtime::new().unwrap()).iter(|| async {
+                let mut client = client.lock().await;
+                let mut app_client = app_client.lock().await;
+                run_wasm_scheduled(&mut client, &mut app_client, &deployment_id).await
+            })
+        },
+    );
+
+    group.bench_with_input(
+        BenchmarkId::new("face wasm direct", "<client>"),
+        &arc_app_client,
+        |b, app_client| {
+            b.to_async(Runtime::new().unwrap()).iter(|| async {
+                let mut app_client = app_client.lock().await;
+                run_wasm_direct(&mut app_client).await
+            })
+        },
+    );
+}
+
+async fn run_wasm_scheduled(
+    client: &mut SchedulerClient<Channel>,
+    detector_client: &mut DetectorClient<Channel>,
+    deployment_id: &str,
+) {
+    let request = LookupRequest {
+        deployment_id: deployment_id.to_owned(),
+        qos: None,
+    };
+
+    let _resp = client.clone().lookup(request).await.unwrap().into_inner();
+
+    // let mut app_client = app::proto::greeter_client::GreeterClient::connect(addr)
+    //     .await
+    //     .unwrap();
+    let request = DetectionRequest {
+        image_png: vec![0u8, 0, 0, 1],
+    };
+    detector_client.run_detection(request).await.unwrap();
+}
+
+async fn run_wasm_direct(detector_client: &mut DetectorClient<Channel>) {
+    let request = DetectionRequest { image_png: vec![] };
+    detector_client.run_detection(request).await.unwrap();
+}
+criterion_group!(benches, bench_face_image, bench_wasm);
 criterion_main!(benches);
