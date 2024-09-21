39d2b7b979b7801f94586a5791578cab713a50bf
kino-ma <ma@kino.ma>
2024-09-21 17:45:04 +0900

    build: build wasm package before mless build

--- build.rs
+++ build.rs
@@ -1,4 +1,25 @@
+use std::path::Path;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/mless.proto")?;
+
+    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("./apps/face-wasm");
+    let path_str = path.display();
+    let pkg = format!("file://{path_str}");
+
+    let output = std::process::Command::new("cargo")
+        .args(["build", "--package", &pkg, "--release"])
+        .output()
+        .expect("Failed to build wasm package");
+
+    if !output.status.success() {
+        let out = String::from_utf8(output.stdout)?;
+        let err = String::from_utf8(output.stderr)?;
+
+        let text = format!("Wasm build failed.\nStdout:\n{out}\n\nStderr:\n{err}");
+
+        return Err(text.into());
+    }
+
     Ok(())
 }
