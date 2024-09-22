ab2ffb93055124221947bda8b15827a1f78786d8
kino-ma <ma@kino.ma>
2024-09-22 20:23:41 +0900

    feat: WasmRunner struct

---
+++ mless-core/src/wasm.rs
@@ -0,0 +1,36 @@
+use std::error::Error;
+
+use wasmer::{imports, Cranelift, Instance, Memory, MemoryType, Module, Store};
+
+pub struct WasmRunner {
+    store: Store,
+    module: Module,
+    memory: Memory,
+    instance: Instance,
+}
+
+impl WasmRunner {
+    pub fn compile(wasm: &[u8]) -> Result<Self, Box<dyn Error>> {
+        let compiler = Cranelift::default();
+
+        let mut store = Store::new(compiler);
+        let module = Module::new(&store, wasm)?;
+
+        let memory = Memory::new(&mut store, MemoryType::new(21, None, false))?;
+
+        let import_object = imports! {
+            "env" => {
+                "memory" => memory.clone(),
+            }
+        };
+
+        let instance = Instance::new(&mut store, &module, &import_object)?;
+
+        Ok(Self {
+            store,
+            module,
+            memory,
+            instance,
+        })
+    }
+}
