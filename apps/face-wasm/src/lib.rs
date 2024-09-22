cd02d77975e5909d7f1909dec2f28c3cc5d01f51
kino-ma <ma@kino.ma>
2024-09-22 11:51:21 +0900

    feat: pass string data each other [wip]

--- apps/face-wasm/src/lib.rs
+++ apps/face-wasm/src/lib.rs
@@ -8,6 +8,10 @@ use core::slice;
 
 use image::{imageops::FilterType, GenericImageView, Pixel};
 
+extern "C" {
+    // fn print(ptr: u32, len: u32);
+}
+
 pub struct DetectionResult {
     _label: String,
     _probability: f32,
@@ -17,13 +21,24 @@ const IMAGE_WIDTH: usize = 224;
 const IMAGE_HEIGHT: usize = 224;
 
 // pub fn main(image_png: &[u8]) -> DetectionResult {
-#[no_mangle]
-pub extern "C" fn main(ptr: i32, len: i32) -> f32 {
+#[cfg_attr(not(test), no_mangle)]
+pub extern "C" fn main(ptr: i32, len: i32) -> i64 {
     let buffer: &[u8] = unsafe { slice::from_raw_parts(ptr as _, len as _) };
 
     let img = image::load_from_memory(buffer);
-    let img = img.unwrap_err();
-    return 1.;
+
+    let msg_start = len as usize + 1;
+
+    let msg = match img {
+        Ok(_img) => "ok".to_owned(),
+        Err(e) => {
+            format!("ERR: Failed to load image: {e}")
+        }
+    };
+
+    write_str(len as usize + 1, &msg);
+    (msg_start as i64) << 32 | msg.len() as i64
+
     // let img = img.resize_to_fill(IMAGE_WIDTH as _, IMAGE_HEIGHT as _, FilterType::Nearest);
 
     // let array = ndarray::Array::from_shape_fn((1, 3, IMAGE_WIDTH, IMAGE_HEIGHT), |(_, c, j, i)| {
@@ -56,4 +71,67 @@ pub extern "C" fn main(ptr: i32, len: i32) -> f32 {
     //     );
     //     println!("details: {:?}", probabilities[i]);
     // }
+}
+
+fn write_str(offset: usize, data: &str) {
+    #[cfg(target_family = "wasm")]
+    grow_to(data.len(), data.len());
+
+    let ptr: *mut u8 = offset as _;
+    unsafe {
+        std::ptr::copy(data.as_ptr(), ptr, data.len());
+    }
+}
+
+#[cfg(target_family = "wasm")]
+const PAGE_SIZE: usize = 65536;
+
+#[cfg(target_family = "wasm")]
+fn grow_to(tail_idx: usize, data_len: usize) -> usize {
+    use core::arch;
+    let current_size = arch::wasm32::memory_size(0);
+    let cap = current_size * PAGE_SIZE;
+    assert!(tail_idx <= cap);
+
+    let start = tail_idx + 1;
+    let available = cap - start;
+    let missing = data_len - available;
+    if missing > 0 {
+        let to_grow = missing / PAGE_SIZE + 1;
+        arch::wasm32::memory_grow(0, to_grow);
+        to_grow
+    } else {
+        0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use image::{imageops::FilterType, GenericImageView};
+
+    use super::*;
+
+    static JPEG: &'static [u8] = include_bytes!("../../../data/pelican.jpeg");
+
+    #[test]
+    fn test_image_load() {
+        let img = image::load_from_memory(JPEG).unwrap();
+
+        let img = img.resize_to_fill(IMAGE_WIDTH as _, IMAGE_HEIGHT as _, FilterType::Nearest);
+
+        let array = ndarray::Array::from_shape_fn(
+            (1, 3, super::IMAGE_WIDTH, IMAGE_HEIGHT),
+            |(_, c, j, i)| {
+                let pixel = img.get_pixel(i as u32, j as u32);
+                let channels = pixel.channels();
+
+                // range [0, 255] -> range [0, 1]
+                (channels[c] as f32) / 255.0
+            },
+        );
+
+        let input = array
+            .as_slice()
+            .expect("failed to convert array into a slice");
+    }
 }
--- apps/face/src/server.rs
+++ apps/face/src/server.rs
@@ -1,8 +1,12 @@
+use core::{slice, str};
 use std::error::Error;
 
 use prost::Message;
 use tonic::{Request, Response, Status};
-use wasmer::{imports, Cranelift, Instance, Module, Store, Value};
+use wasmer::{
+    imports, AsStoreRef, Cranelift, Function, FunctionEnv, FunctionEnvMut, Instance, Module, Store,
+    Value,
+};
 
 use crate::proto::{
     detector_server::Detector, DetectionReply, DetectionRequest, InferReply, InferRequest,
@@ -62,7 +66,24 @@ impl Detector for FaceServer {
         let module = Module::new(&store, WASM)
             .map_err(|e| Status::aborted(format!("Failed to create WebAssembly module: {e}")))?;
 
-        let import_object = imports! {};
+        struct MyEnv;
+        let env = FunctionEnv::new(&mut store, MyEnv);
+
+        fn print_str(ptr: u32, len: u32) {
+            println!("print_str called!!");
+            println!("ptr: {ptr}, len: {len}");
+            let slic: &[u8] = unsafe { slice::from_raw_parts(ptr as _, len as _) };
+            let text = str::from_utf8(slic).unwrap();
+            println!("{text}");
+        }
+        // let print_typed = Function::new_typed_with_env(&mut store, &env, print_str);
+
+        let import_object = imports! {
+            "env" => {
+                // "print" => print_typed,
+            }
+        };
+
         let instance = Instance::new(&mut store, &module, &import_object)
             .map_err(|e| Status::aborted(format!("Failed to create WebAssembly instance: {e}")))?;
 
@@ -90,11 +111,32 @@ impl Detector for FaceServer {
             .call(&mut store, params)
             .map_err(|e| Status::aborted(format!("Failed to call WebAssembly function: {e}")))?;
 
-        let value = out[0].unwrap_f32();
+        let value = out[0].unwrap_i64();
+        println!("value: {value:x}");
+
+        let start = value >> 32;
+        let len = value & 0xffff_ffff;
+
+        let memory = instance.exports.get_memory("memory").map_err(|e| {
+            Status::aborted(format!(
+                "Failed to get expported WebAssembly memory after call: {e}"
+            ))
+        })?;
+
+        let view = memory.view(&mut store);
+        let mut buffer = Vec::new();
+        view.read(start as _, &mut buffer).map_err(|e| {
+            Status::aborted(format!("Failed to read WebAssembly memory after call: {e}"))
+        })?;
+        let msg = core::str::from_utf8(&buffer).map_err(|e| {
+            Status::aborted(format!("Failed to read WebAssembly memory after call: {e}"))
+        })?;
+        println!("Message returned: {msg}");
+        println!("Raw: {buffer:?}");
 
         let reply = DetectionReply {
             label: "EXECUTED!".to_owned(),
-            probability: value,
+            probability: 42.,
         };
 
         Ok(Response::new(reply))
