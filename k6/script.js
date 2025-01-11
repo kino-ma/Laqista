bf6bd4141c4d0d35ca88d47f732b9388d7b82500
kino-ma <ma@kino.ma>
2025-01-11 16:53:19 +0900

    docs: benchmark result desktop from server

--- k6/script.js
+++ k6/script.js
@@ -3,8 +3,8 @@ import { Counter } from "k6/metrics";
 import { Client, StatusOK } from "k6/net/grpc";
 import { check } from "k6";
 
-const SCHEDULER = "133.27.186.106:50051";
-const SCHEDULER_URL = "http://133.27.186.106:50051";
+const SCHEDULER     =        "133.27.171.58:50051";
+const SCHEDULER_URL = "http://133.27.171.58:50051";
 
 const NAN1 = "http://133.27.171.130:50051";
 const NAN2 = "http://133.27.171.130:50052";
